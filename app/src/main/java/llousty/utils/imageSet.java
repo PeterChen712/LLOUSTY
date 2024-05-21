be75f08802ee31269a0d8ef13decd2f584ee315b
PeterChen712 <rudypeter@gmail.com>
2024-05-22 01:19:06 +0800

    feat : create imageSet.java

---
+++ app/src/main/java/llousty/utils/imageSet.java
@@ -0,0 +1,13 @@
+package llousty.utils;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class imageSet {
+    public static ImageView setImages(String url, double width, double height){
+        ImageView imageView = new ImageView(new Image(url));
+        imageView.setFitWidth(width); 
+        imageView.setFitHeight(height);
+        return imageView;
+    }
+}
