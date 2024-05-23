fe35075dbd8c5debe58742a17e8dcbc46f037a19
PeterChen712 <rudypeter@gmail.com>
2024-05-23 10:22:23 +0800

    feat homescene

---
+++ app/src/main/java/llousty/scene/HomeScene.java
@@ -0,0 +1,11 @@
+package llousty.scene;
+
+import javafx.stage.Stage;
+
+public class HomeScene {
+    private Stage stage;
+
+    public HomeScene(Stage stage) {
+        this.stage = stage;
+    }
+}
